7ef7d71003b2b4e9d8c6a8e967062e72083e7033
Nishchal Arora <narora88@gmail.com>
2020-10-26 15:07:56 +1000

    Adding tooltip on hover
    Adding tooltip functionality on hover, displaying business name and address

--- MyBusinessVis.js
+++ MyBusinessVis.js
@@ -2,22 +2,16 @@ define('MyBusinessVis', ['ol', 'jquery'], function(ol, $) {
     function visualizeBusinesses(instanceData) {
         var iconFeatures = [];
 
-        var iconStyle = new ol.style.Style({
-            image: new ol.style.Icon({
-                src: 'http://localhost:8081/mybusinessvis/data/icon.png',
-                scale: 0.1
-            })
-        });
-
         if (instanceData.series[0]) {
-            $.each(instanceData.series[0], function(index, business) {
+            $.each(instanceData.series[0], $.proxy(function(index, business) {
                 var iconFeature = new ol.Feature({
                     geometry: new ol.geom.Point(ol.proj.transform([business.lon, business.lat], 'EPSG:4326', 'EPSG:3857')),
-                    name: business.name
+                    name: business.name,
+                    address: business.address
                 });
-                iconFeature.setStyle(iconStyle);
+                iconFeature.setStyle(this.iconStyle);
                 iconFeatures.push(iconFeature);
-            });
+            }, this));
         }
 
         var vectorSource = new ol.source.Vector({
@@ -36,6 +30,35 @@ define('MyBusinessVis', ['ol', 'jquery'], function(ol, $) {
         window.document.getElementById(instanceData.id).appendChild(svg);
         svg.style.display = "none";
 
+        this.iconStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: 'http://localhost:8081/mybusinessvis/data/icon.png',
+                scale: 0.1
+            })
+        });
+
+        this.highlightStyle = new ol.style.Style({
+            image: new ol.style.Icon({
+                src: 'http://localhost:8081/mybusinessvis/data/icon_hover.png',
+                scale: 0.1
+            })
+        });
+
+        var selected = null;
+        var container = $("<div id='popup' class='ol-popup'></div>");
+        var content = $("<div id='popup-content'></div>");
+        
+        document.body.append(container);
+       	container.append(content);
+
+       	var overlay = new ol.Overlay({
+            element: container[0],
+            autoPan: true,
+            autoPanAnimation: {
+                duration: 250
+            }
+        });
+        
         this.map = new ol.Map({
             target: instanceData.id,
             size: [instanceData.width, instanceData.height],
@@ -44,6 +67,7 @@ define('MyBusinessVis', ['ol', 'jquery'], function(ol, $) {
                     source: new ol.source.OSM()
                 })
             ],
+            overlays: [overlay],
             view: new ol.View({
                 center: ol.proj.fromLonLat([0, 0]),
                 zoom: 1
@@ -51,5 +75,26 @@ define('MyBusinessVis', ['ol', 'jquery'], function(ol, $) {
         });
 
         visualizeBusinesses(instanceData);
+
+        this.map.on('pointermove', $.proxy(function(e) {
+            this.map.forEachFeatureAtPixel(e.pixel, $.proxy(function(f) {
+                if (selected !== null) {
+                    selected.setStyle(iconStyle);
+                    selected = null;
+                }
+
+                if (f instanceof ol.Feature) {
+                    selected = f;
+                    f.setStyle(highlightStyle);
+                    content[0].innerHTML = '<div style="font-family: Helvetica; font-size: medium; color:#0D4D93; margin-bottom: 5px;"><span style="font-weight: bold;">Name: </span>' + f.getProperties().name + '</div>';
+                    content[0].innerHTML += '<div style="font-family: Helvetica; font-size: medium; color:#0D4D93;"><span style="font-weight: bold;">Address: </span>' + f.getProperties().address + '</div>';
+                    overlay.setPosition(f.getGeometry().getCoordinates());
+                    return true;
+                } else {
+                    overlay.setPosition(undefined);
+                    return false;
+                }
+            }, this));
+       }, this));
     };
 });
